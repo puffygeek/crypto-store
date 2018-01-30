88c219726d9d52b57da93142e6028856b46a686e
Federico Ulfo <federicoulfo@gmail.com>
2018-01-30 13:49:28 -0500

    Test skeleton

---
+++ test/library/GoogleStorageTest.js
@@ -0,0 +1,13 @@
+const Store = artifacts.require('./Store');
+
+describe('GoogleStorageService', async function() {
+
+  describe('put', async function() {
+
+  });
+
+  describe('get', async function() {
+
+  });
+
+});
