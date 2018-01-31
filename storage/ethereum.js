31fa52e1d29c3a5d9b9a5de5131683202a8aad5a
Sagiv Ofek <sagiv4@gmail.com>
2018-01-31 11:59:51 -0500

    extra space

--- storage/ethereum.js
+++ storage/ethereum.js
@@ -5,6 +5,7 @@ const storeSol = require('../build/contracts/Store.json');
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:9545'));
 const StoreContract = TruffleContract(storeSol);
 StoreContract.setProvider(web3.currentProvider);
+
 const set = async (ns, key, value, callerAdrs) => {
   if (!ns || !key || !value) return null;
   const storeContract = await StoreContract.deployed();
